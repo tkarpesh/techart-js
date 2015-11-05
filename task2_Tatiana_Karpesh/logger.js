6bba51ecbff0ce7f5f6d4d179e89c03f963a4bd5
tkarpesh <tatiana.karpesh@gmail.com>
2015-11-06 02:56:11 +0300

    Add all functionality

--- task2_Tatiana_Karpesh/logger.js
+++ task2_Tatiana_Karpesh/logger.js
@@ -1,47 +1,57 @@
-function Logger(level, strategy) {
-  this.LEVELS = ['DEBUG', 'WARN', 'ERROR'];
+(function() {
+  function Logger(level, strategy) {
+    this.LEVELS = ['DEBUG', 'WARN', 'ERROR'];
 
-  this._strategy = strategy;
-  this._currentLevel = this.LEVELS.indexOf(level) == -1 ? this.LEVELS[0] : level;
-};
+    this._strategy = strategy;
+    this._currentLevel = this.LEVELS.indexOf(level) == -1 ? this.LEVELS[0] : level;
+  };
 
-Logger.prototype.log = function(level, message) {
-  if (this._isPrintLog(level)) {
-    this._strategy.call(this, this._formattedMessage(level, message));
-  }
-};
+  Logger.prototype.log = function(level, message) {
+    if (this._isPrintLog(level)) {
+      this._strategy.call(this, this._formattedMessage(level, message));
+    }
+  };
 
-Logger.prototype._formattedMessage = function(level, message) {
-  return '[' + this._formattedCurrentTime() + '](' + level + ') ' + message;
-};
+  Logger.prototype._formattedMessage = function(level, message) {
+    return '[' + this._formattedCurrentTime() + '](' + level + ') ' + message;
+  };
 
-Logger.prototype._formattedCurrentTime = function() {
-  return (new Date()).toUTCString();
-};
+  Logger.prototype._formattedCurrentTime = function() {
+    return (new Date()).toUTCString();
+  };
 
-Logger.prototype._isPrintLog = function(level) {
-  return this.LEVELS.indexOf(this._currentLevel) <= this.LEVELS.indexOf(level);
-};
+  Logger.prototype._isPrintLog = function(level) {
+    return this.LEVELS.indexOf(this._currentLevel) <= this.LEVELS.indexOf(level);
+  };
 
+  Logger.isNode = function() {
+    return typeof module !== 'undefined' && module.exports;
+  };
 
-var consoleStrategy = function(level, message) {
-  console.log(level, message);
-}
+  Logger.consoleStrategy = function(message) {
+    console.log(message);
+  };
 
-var alertStrategy = function(level, message) {
-  alert(level, message);
-}
+  Logger.alertStrategy = function(message) {
+    if (!Logger.isNode()) {
+      alert(message);
+    }
+  };
 
-var windowStrategy = function(level, message) {
-  document.write(level, message);
-}
+  Logger.windowStrategy = function(message) {
+    if (!Logger.isNode()) {
+      document.write(message);
+    }
+  };
 
-var apiServerStrategy = function(level, message) {
-  var xhr = new XMLHttpRequest();
+  Logger.apiServerStrategy = function(message) {
+    if (typeof XMLHttpRequest !== 'undefined') {
+      var xhr = new XMLHttpRequest();
 
-  xhr.open('POST', 'http://example.com', true);
-  xhr.send(message);
-}
+      xhr.open('POST', 'http://httpbin.org/post', true);
+      xhr.send(message);
+    }
+  };
 
-a = new Logger('WARN', consoleStrategy);
-a.log('ERROR', 'You need to use getDate() to get the date part. The getDay() function returns the day number');
+  (Logger.isNode() ? global : window).Logger = Logger;
+})();
